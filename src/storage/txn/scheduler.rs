0b1fc070f9ebb631c4089fa27fe8f5207c0d85bd
cfzjywxk <lsswxrxr@163.com>
2023-06-02 22:14:29 +0800

    use must_call for async write callback and add panic for unexpected path
    Signed-off-by: cfzjywxk <lsswxrxr@163.com>

--- src/storage/txn/scheduler.rs
+++ src/storage/txn/scheduler.rs
@@ -831,6 +831,11 @@ impl<E: Engine, L: LockManager> Scheduler<E, L> {
                 Err(e) => {
                     if !Self::is_undetermined_error(&e) {
                         do_wake_up = false;
+                    } else {
+                        panic!(
+                            "undetermined error: {:?} cid={}, tag={}, process result={:?}",
+                            e, cid, tag, &pr
+                        );
                     }
                     ProcessResult::Failed {
                         err: StorageError::from(e),
@@ -1026,10 +1031,14 @@ impl<E: Engine, L: LockManager> Scheduler<E, L> {
             .unwrap();
     }
 
-    fn is_undetermined_error(_e: &tikv_kv::Error) -> bool {
-        // TODO: If there's some cases that `engine.async_write` returns error but it's
-        // still possible that the data is successfully written, return true.
-        false
+    // Return true if raftstore returns error and the underlying write status could
+    // not be decided.
+    fn is_undetermined_error(e: &tikv_kv::Error) -> bool {
+        if let tikv_kv::ErrorInner::Request(error_header) = &*(e.0) {
+            error_header.message == "async write on_applied callback is dropped"
+        } else {
+            false
+        }
     }
 
     fn early_response(
@@ -1498,7 +1507,7 @@ impl<E: Engine, L: LockManager> Scheduler<E, L> {
             }
         }
         // If it's not finished while the channel is closed, it means the write
-        // is undeterministic. in this case, we don't know whether the
+        // status is undetermined. in this case, we don't know whether the
         // request is finished or not, so we should not release latch as
         // it may break correctness.
         panic!(
